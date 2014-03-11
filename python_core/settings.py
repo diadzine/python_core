c8312d5e6c38b215d6e9a43477f80b0b28a83d91
votre_pseudo <moi@email.com>
2014-03-11 20:03:18 +1100

    Added pip requirements file, added django cors app.

--- python_core/settings.py
+++ python_core/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_bcrypt',
+    #Additionnal Apps
+    'corsheaders',
+    # Custom Apps
     'news',
     'users'
 )
