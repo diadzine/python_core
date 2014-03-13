246f8dc5fcf5a71db6ceda7c21b700bded6f62e9
seba-1511 <seba-1511@hotmail.com>
2014-03-13 22:48:08 +1100

    fixed typo in models.

--- blogs/views.py
+++ blogs/views.py
@@ -10,10 +10,10 @@ from users.views import isLoggedIn
 def bloggers(request):
     if request.GET.get('id'):
         blogsId = request.GET.get('id')
-        blogs = News.objects.filter(id=blogsId)
+        blogs = Bloggers.objects.filter(id=blogsId)
 
     else:
-        blogs = News.objects.all().order_by('date').reverse()[:20]
+        blogs = Bloggers.objects.all().order_by('date').reverse()[:20]
 
     return HttpResponse(serialize('json', blogs))
 
@@ -22,9 +22,9 @@ def posts(request):
 	# Handle blogId and postId case.
     if request.GET.get('id'):
         postId = request.GET.get('id')
-        post = News.objects.filter(id=postId)
+        post = BlogPosts.objects.filter(id=postId)
 
     else:
-        post = News.objects.all().order_by('date').reverse()[:20]
+        post = BlogPosts.objects.all().order_by('date').reverse()[:20]
 
     return HttpResponse(serialize('json', post))
