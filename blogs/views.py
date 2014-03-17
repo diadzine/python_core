25150ef886774ddd68b3b224f13d5bdbbe4f1e4c
seba-1511 <seba-1511@hotmail.com>
2014-03-17 17:57:00 +1100

    wrote save function for bloggers and blgoposts.

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@
 
 from django.http import HttpResponse, Http404
 from django.core.serializers import serialize
+from django.views.decorators.csrf import csrf_exempt
 
 from blogs.models import Bloggers, BlogPosts
 from users.views import isLoggedIn
@@ -30,11 +31,55 @@ def posts(request):
     return HttpResponse(serialize('json', post))
 
 
+@csrf_exempt
 def saveBlogger(request):
-    return True
+    if isLoggedIn(request):
+        req = request.POST
+        id = req.get('id')
+        name = req.get('name')
+        biography = req.get('biography')
+        linkResults = req.get('linkResults')
+        profilePic = req.get('profilePic')
+        sponsors = req.get('sponsors')
+        ad = req.get('ad')
+        if not int(id) == 0:
+            blogger = Bloggers.objects.filter(id=id).first()
+        else:
+            blogger = Bloggers()
+        if name:
+            blogger.name = name
+            blogger.biography = biography
+            blogger.linkResults = linkResults
+            blogger.profilePic = profilePic
+            blogger.sponsors = sponsors
+            blogger.ad = ad
+            blogger.save()
+            id = blogger.id
+            blogger = Bloggers.objects.filter(id=id)
+            return HttpResponse(serialize('json', blogger))
+    return HttpResponse('0')
 
+@csrf_exempt
 def savePost(request):
-    return True
+    if isLoggedIn(request):
+        req = request.POST
+        id = req.get('id')
+        title = req.get('title')
+        content = req.get('content')
+        blogId = req.get('blogId')
+        if not int(id) == 0:
+            post = BlogPosts.objects.filter(id=id).first()
+        else:
+            post = BlogPosts()
+        if title and content and blogId:
+            post.title = title
+            post.content = content
+            post.blogId = blogId
+            post.save()
+            id = post.id
+            post = BlogPosts.objects.filter(id=id)
+            return HttpResponse(serialize('json', post))
+    return HttpResponse('0')
 
 def deleteBlogger(request):
     bloggerId = int(request.GET.get('id'))
