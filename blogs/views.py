91aa00df5a478e12b0fd86a9849ba6540a683759
seba-1511 <seba-1511@hotmail.com>
2014-03-17 17:10:33 +1100

    implemeted code to delete post and bloggers.

--- blogs/views.py
+++ blogs/views.py
@@ -28,15 +28,28 @@ def posts(request):
         post = BlogPosts.objects.all().order_by('date').reverse()[:20]
 
     return HttpResponse(serialize('json', post))
-    
+
 
 def saveBlogger(request):
-
+    return True
 
 def savePost(request):
-
+    return True
 
 def deleteBlogger(request):
-
+    bloggerId = int(request.GET.get('id'))
+    if bloggerId:
+        if isLoggedIn(request):
+            blogger = Bloggers.objects.filter(id=bloggerId)
+            blogger.delete()
+            return HttpResponse('1')
+    return HttpResponse('0')
 
 def deletePost(request):
+    postId = int(request.GET.get('id'))
+    if postId:
+        if isLoggedIn(request):
+            post = BlogPosts.objects.filter(id=postId)
+            post.delete()
+            return HttpResponse('1')
+    return HttpResponse('0')
