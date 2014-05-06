faad09b2f63d84f8f2de79694ce5b194253027d1
seba-1511 <seba-1511@hotmail.com>
2014-05-06 16:42:30 +0200

    Added default spider files.

---
+++ rankings/fis/fis/spiders/races.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from scrapy.selector import Selector
+
+from scrapy.spider import BaseSpider
+from scrapy.http import Request
+
+from tooski.items import TooskiNews
+
+
+class MyCrawlerSpider(BaseSpider):
+    # nom du crawler à spécifier lors de l'exécution
+    name = 'tooski'
+
+    # domaine(s) sur le ou lesquels le crawler aura le droit d'aller
+    allowed_domains = ['data.fis-ski.com']
+
+    # We'll save this kind of variable in a pickle file and load it each time.
+    max_newsid = 16015
+
+    def start_requests(self):
+        for i in xrange(16000, self.max_newsid):
+            yield Request(
+                'http://data.fis-ski.com/dynamic/results.html?sector=AL&raceid=%d' % i,
+                callback=self.parse_item)
+
+    def parse_item(self, response):
+        # TODO: Save the table in json/string format as a variable of item.
+        hxs = Selector(response)
+        item = TooskiNews()
+        url = response.url
+        item['link'] = url
+        item['id'] = url[url.index('raceid=') + 7:]
+        item['date'] = hxs.xpath(
+            '//div[contains(@class, "padding-content")]/h3/span/text()').extract()[0].strip()
+        item['location'] = hxs.xpath(
+            '//div[contains(@class, "padding-content")]/h3/a/text()').extract()[0].strip()
+
+        info = hxs.xpath(
+            '//div[contains(@class, "padding-content")]/div/div/h4/text()').extract()[0].strip()
+        item['info'] = info
+        item['genre'] = 'H' if 'Men' in info else 'F'
+
+        if 'World Cup' in info:
+            item['category'] = 'WC'
+        elif 'European Cup' in info:
+            item['category'] = 'EC'
+        elif 'FIS Race' in info:
+            item['category'] = 'FIS'
+        elif 'Citizen' in info:
+            item['category'] = 'CIT'
+        elif 'National Junior' in info:
+            item['category'] = 'NJR'
+        elif 'Junior' in info:
+            item['category'] = 'JR'
+        else:
+            item['category'] = 'Other'
+
+        if 'Giant Slalom' in info:
+            item['discipline'] = 'GS'
+        elif 'Downhill' in info:
+            item['discipline'] = 'DH'
+        elif 'Slalom' in info:
+            item['discipline'] = 'SL'
+        elif 'Super G' in info:
+            item['discipline'] = 'SG'
+        elif 'Super Combined' in info:
+            item['dicipline'] = 'SC'
+        else:
+            item['discipline'] = 'Other'
+
+        return item
---
+++ rankings/fis/fis/spiders/ranking.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from scrapy.selector import Selector
+
+from scrapy.spider import BaseSpider
+from scrapy.http import Request
+
+from tooski.items import TooskiRanking
+
+
+class RankingSpider(BaseSpider):
+    # nom du crawler à spécifier lors de l'exécution
+    name = 'ranking'
+
+    # domaine(s) sur le ou lesquels le crawler aura le droit d'aller
+    allowed_domains = ['www.fis-ski.com']
+
+    def start_requests(self):
+        # When saving, the file should be moved to the root of /website.
+        # So that the js can parse, it is directly served by Apache and doesn't
+        # require connection to th DB. Use shutil.copy(src, dest)
+        yield Request('http://www.fis-ski.com/alpine-skiing/leader-board/',
+                      callback=self.parse_item)
+
+    def parse_item(self, response):
+        hxs = Selector(response)
+        item = TooskiRanking()
+        item['link'] = response.url
+
+        rows = hxs.xpath('//div[contains(@class, "dcm-leaderBoard")]//tr')
+        men = []
+        women = []
+        men_flag = 0
+        for row in rows:
+            name = row.xpath(
+                'td/div/a/span[contains(@class, "dcm-athName")]/text()').extract()
+            country = row.xpath(
+                'td/div/div[contains(@class, "dcm-noc")]/text()').extract()
+            row = row.xpath('td/text()').extract()
+            if row and int(row[0]) <= 25:
+                place = row[0]
+                score = row[2]
+                name = name[0]
+                country = country[0]
+                if int(row[0]) == 1:
+                    men_flag += 1
+                if men_flag >= 2:
+                    men.append([place, name, country, score])
+                else:
+                    women.append([place, name, country, score])
+
+        item['men'] = men
+        item['women'] = women
+        return item
