74da447ee5c203011113b5ca1bed199c68fb65c4
seba-1511 <seba-1511@hotmail.com>
2014-05-07 16:31:21 +0200

    structured scraper for server archtecture.

--- rankings/scraper.py
+++ rankings/scraper.py
@@ -4,7 +4,8 @@ import shutil
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 BACKEND_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
-WEBAPPS_DIR = os.path.abspath(os.path.join(BACKEND_DIR, os.pardir))
+APP_DIR = os.path.abspath(os.path.join(BACKEND_DIR, os.pardir))
+WEBAPPS_DIR = os.path.abspath(os.path.join(APP_DIR, os.pardir))
 
 # We get the leaederboard rankings and move them to the Apache server:
 os.system('cd ' + CURRENT_DIR +
