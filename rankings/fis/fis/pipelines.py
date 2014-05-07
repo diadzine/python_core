50bf8da0406fa7439ebeed226bc813b33f0fbab1
seba-1511 <seba-1511@hotmail.com>
2014-05-07 16:49:42 +0200

    testing.

--- rankings/fis/fis/pipelines.py
+++ rankings/fis/fis/pipelines.py
@@ -3,21 +3,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import os
-import sys
-from time import strptime, mktime
-CURRENT_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-SCRAPY_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
-MODULE_DIR = os.path.abspath(os.path.join(SCRAPY_DIR, os.pardir))
-BACKEND_DIR = os.path.abspath(os.path.join(MODULE_DIR, os.pardir))
-WEBAPPS_DIR = os.path.abspath(os.path.join(BACKEND_DIR, os.pardir))
+# import os
+# import sys
+# from time import strptime, mktime
+# CURRENT_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+# SCRAPY_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
+# MODULE_DIR = os.path.abspath(os.path.join(SCRAPY_DIR, os.pardir))
+# BACKEND_DIR = os.path.abspath(os.path.join(MODULE_DIR, os.pardir))
+# WEBAPPS_DIR = os.path.abspath(os.path.join(BACKEND_DIR, os.pardir))
 
-sys.path.append(BACKEND_DIR)
-sys.path.append(MODULE_DIR)
-sys.path.append(WEBAPPS_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'python_core.settings'
+# sys.path.append(BACKEND_DIR)
+# sys.path.append(MODULE_DIR)
+# sys.path.append(WEBAPPS_DIR)
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'python_core.settings'
 
-from rankings.models import Races
+# from rankings.models import Races
 
 
 class FisPipeline(object):
@@ -27,15 +27,15 @@ class FisPipeline(object):
             return item
 
         # Here we'll register the current item in the database:
-        race = Races()
-        race.info = item['info'].strip()
-        race.category = item['category'].strip()
-        race.genre = item['genre'].strip()
-        race.link = item['link'].strip()
-        race.location = item['location'].strip()
-        race.discipline = item['discipline'].strip()
-        race.raceId = item['id']
-        race.table = item['table'].strip()
-        race.date = mktime(strptime(item['date'].strip(), '%d.%m.%Y'))
-        race.save()
+        # race = Races()
+        # race.info = item['info'].strip()
+        # race.category = item['category'].strip()
+        # race.genre = item['genre'].strip()
+        # race.link = item['link'].strip()
+        # race.location = item['location'].strip()
+        # race.discipline = item['discipline'].strip()
+        # race.raceId = item['id']
+        # race.table = item['table'].strip()
+        # race.date = mktime(strptime(item['date'].strip(), '%d.%m.%Y'))
+        # race.save()
         return item
