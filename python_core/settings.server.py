b20b9717deab33c4e1dbfd7398ed0212fdebe5b3
seba-1511 <seba-1511@hotmail.com>
2014-07-30 14:30:51 +0200

    Implemented server settings.

---
+++ python_core/settings.server.py
@@ -0,0 +1,146 @@
+"""
+Django settings for python_core project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+CURRENT_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+
+APPEND_SLASH = True
+
+FORCE_SCRIPT_NAME = '/api'
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '<secret_key>'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['*.tooski.ch', '*.webfaction.com']
+
+ADMINS = (
+    ('Sebastien Arnold', 'seba-1511@hotmail.com'),
+    # ('Your Name', 'your_email@example.com'),
+)
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Additionnal Apps
+    'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+    # 'corsheaders',
+    # Custom Apps
+    'news',
+    'users',
+    'pages',
+    'blogs',
+    'ads',
+    'rankings',
+    'pictures',
+    'skiclubs',
+    'widgets',
+    'angulation',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
+    # Django Cors:
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+ROOT_URLCONF = 'python_core.urls'
+
+WSGI_APPLICATION = 'python_core.wsgi.application'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_tooski',
+        'USER': 'application',
+        'PASSWORD': '<db_password>',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# Cache
+# http://docs.webfaction.com/software/django/config.html#configuring-django-to-use-memcached
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'unix:/home/tooski/memcached.sock',
+    }
+}
+
+
+# URLs
+ROOT_URLCONF = 'python_core.urls'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'fr-ch'
+
+TIME_ZONE = 'CET'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
