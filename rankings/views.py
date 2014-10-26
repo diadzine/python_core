4a622b7d6f8fbdd1b3ad496ffd367f1c825bdf04
seba-1511 <seba-1511@hotmail.com>
2014-10-25 20:55:02 -0700

    Implemented efficient api for rankings.

--- rankings/views.py
+++ rankings/views.py
@@ -1,45 +1,46 @@
 #-*- coding: utf-8 -*-
 import ujson
-import time
 
-from django.http import HttpResponse
-from django.core.serializers import serialize
+from django.http import HttpResponse, Http404
 from django.db import connection
 
-from rankings.models import Races
-
-RACES_PER_VIEW = 15
-
-
-def get(request):
-    if request.GET.get('id'):
-        racesId = request.GET.get('id')
-        races = Races.objects.filter(id=racesId)
-
-    else:
-        races = Races.objects.all().order_by('date').reverse()[:25]
-
-    return HttpResponse(serialize('json', races))
+RACES_PER_VIEW = 25
 
 
 def last_races(request):
-    races = Races.objects.all().order_by('date').reverse()[:RACES_PER_VIEW]
-    return HttpResponse(
-        serialize('json', races),
+    cursor = connection.cursor()
+    query = "SELECT id, info, category, genre, link, location, discipline, raceId, date FROM rankings_races ORDER BY date DESC LIMIT " + \
+        str(RACES_PER_VIEW) + ";"
+    cursor.execute(query)
+    races = dictfetchall(cursor)
+    races = ujson.dumps(races, encode_html_chars=False, ensure_ascii=False)
+    res = HttpResponse(
+        races,
         content_type="application/json"
     )
+    return res
 
 
 def race(request, pk):
-    race = Races.objects.filter(pk=pk).first()
-    race = serialize('json', [race], ensure_ascii=False)
-    race = race[1:-1]
-    return HttpResponse(race, content_type="application/json")
+    pk = str(int(pk))
+    cursor = connection.cursor()
+    query = "SELECT id, info, category, genre, link, location, discipline, `table`, raceId, date FROM rankings_races WHERE id='" + pk + "';"
+    cursor.execute(query)
+    races = dictfetchall(cursor)[0]
+    races = ujson.dumps(races, encode_html_chars=False, ensure_ascii=False)
+    res = HttpResponse(
+        races,
+        content_type="application/json"
+    )
+    return res
 
 
 def race_category(request, category):
+    if category not in ['WC', 'EC', 'FIS']:
+        return Http404
     cursor = connection.cursor()
-    query = "SELECT id, info, category, genre, link, location, discipline,`table`, raceId, date FROM rankings_races WHERE category='WC' ORDER BY date DESC LIMIT 10;"
+    query = "SELECT id, info, category, genre, link, location, discipline, raceId, date FROM rankings_races WHERE category='" + \
+        category + "' ORDER BY date DESC LIMIT " + str(RACES_PER_VIEW) + ";"
     cursor.execute(query)
     races = dictfetchall(cursor)
     races = ujson.dumps(races, encode_html_chars=False, ensure_ascii=False)
