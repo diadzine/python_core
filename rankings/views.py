08a274c17d474748bbaf84d60d9aa8dc566e6899
seba-1511 <seba-1511@hotmail.com>
2014-10-25 21:18:38 -0700

    Implemented paging in categories.

--- rankings/views.py
+++ rankings/views.py
@@ -2,6 +2,7 @@
 import ujson
 
 from django.http import HttpResponse, Http404
+from django.core.cache import cache
 from django.db import connection
 
 RACES_PER_VIEW = 25
@@ -24,7 +25,8 @@ def last_races(request):
 def race(request, pk):
     pk = str(int(pk))
     cursor = connection.cursor()
-    query = "SELECT id, info, category, genre, link, location, discipline, `table`, raceId, date FROM rankings_races WHERE id='" + pk + "';"
+    query = "SELECT id, info, category, genre, link, location, discipline, `table`, raceId, date FROM rankings_races WHERE id='" + \
+        pk + "';"
     cursor.execute(query)
     races = dictfetchall(cursor)[0]
     races = ujson.dumps(races, encode_html_chars=False, ensure_ascii=False)
@@ -38,11 +40,28 @@ def race(request, pk):
 def race_category(request, category):
     if category not in ['WC', 'EC', 'FIS']:
         return Http404
-    cursor = connection.cursor()
-    query = "SELECT id, info, category, genre, link, location, discipline, raceId, date FROM rankings_races WHERE category='" + \
-        category + "' ORDER BY date DESC LIMIT " + str(RACES_PER_VIEW) + ";"
-    cursor.execute(query)
-    races = dictfetchall(cursor)
+    page = request.GET.get('page')
+    races = None
+    page = 0 if page is None else page
+    offset = RACES_PER_VIEW * page
+
+    if page is not None and int(page) in [1, 2, 3, 4]:
+        page = int(page)
+        key = 'race' + category
+        start = 0
+        end = (5 * RACES_PER_VIEW)
+        races = cache.get(key)[start:end]
+
+    if races is None:
+        cursor = connection.cursor()
+        query = "SELECT id, info, category, genre, link, location, discipline, raceId, date FROM rankings_races WHERE category='" + \
+            category + "' ORDER BY date DESC LIMIT " + \
+                str(offset) + ", " + str(RACES_PER_VIEW) + ";"
+        cursor.execute(query)
+        races = dictfetchall(cursor)
+        if page is not None and int(page) in [1, 2, 3, 4]:
+            cache.set(key, races, 3600)
+
     races = ujson.dumps(races, encode_html_chars=False, ensure_ascii=False)
     res = HttpResponse(
         races,
