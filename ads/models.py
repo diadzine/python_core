67f9f6fc331330686007069430f124bd96ac5673
Aymeric Bringard <contact@aymericb.com>
2016-03-07 23:07:00 +0100

    add AdsManager to Ads model

--- ads/models.py
+++ ads/models.py
@@ -3,6 +3,41 @@ from django.conf import settings
 from multiselectfield import MultiSelectField
 
 
+class AdsManager(models.Manager):
+    def get_queryset(self):
+        return super(AdsManager, self).get_queryset()
+
+    def by_placeholder(self, placeholder):
+        return super(AdsManager, self).get_queryset().filter(
+            placeholders__contains=placeholder
+        )
+
+    def by_category(self, category):
+
+        if category == 'square':
+            qs = super(AdsManager, self).get_queryset().filter(square=1)
+        elif category == 'horizontal':
+            qs = super(AdsManager, self).get_queryset().filter(horizontal=1)
+        elif category == 'vertical':
+            qs = super(AdsManager, self).get_queryset().filter(vertical=1)
+        else:
+            qs = super(AdsManager, self).get_queryset()
+
+        return qs
+
+    def by_placeholder_and_category(self, placeholder, category):
+        criteria = {
+            'placeholders__contains': placeholder,
+            category: 1,
+        }
+        qs = super(AdsManager, self).get_queryset().filter(**criteria)
+
+        if not len(qs):
+            qs = self.by_category(category)
+
+        return qs
+
+
 class Ads(models.Model):
     name = models.CharField(max_length=510)
     url = models.TextField(null=True)
@@ -20,6 +55,8 @@ class Ads(models.Model):
         auto_now_add=True,
         auto_now=False
     )
+
+    objects = AdsManager()
 
     def __unicode__(self):
         return u'%s' % self.name
